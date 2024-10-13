87afe78def832a4a4490a9b39e457b91877db8fd
OmarHany-sudo <S23511739EG@Std.aou.edu.eg>
2024-10-13 15:30:04 +0300

    main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+        duration: 1000, // سرعة التأثيرات
+        once: true, // تشغيل التأثيرات مرة واحدة فقط
+    });
+});
